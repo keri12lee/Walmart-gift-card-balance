ac1e4c6a3b2b0ca18825aa87d18b46ffc8055573
keri12lee <keri12lee@outlook.com>
2026-08-06 16:44:59 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header(){
+return(
+<header>
+<h1>Walmart Gift Card Balance</h1>
+<p>Manage your gift card records easily</p>
+</header>
+);
+}
+
+export default Header;
